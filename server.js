493890db87f4aa297637318bf17a83cf4e66013c
memo-ibrahim-alean <mohamed.ibrahim.alean@gmail.com>
2025-04-23 04:30:32 +0200

    setup project structure and basic Express server

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
